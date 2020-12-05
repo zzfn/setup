15af3f41753a8a3ed6523aa7ac77b7f1f405c44b
zzfn <chenc@fengyuntec.com>
2020-12-05 20:25:01 +0800

    1

---
+++ gateway/src/main/java/com/zzfn/GateWayApplication.java
@@ -0,0 +1,17 @@
+package com.zzfn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zzfn
+ * @date 2020-12-03 10:26 下午
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
