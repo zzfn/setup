67106c705892e9c99d05a9e39cfe4eb14f9c12f6
zzfn <chenc@fengyuntec.com>
2020-12-06 22:20:39 +0800

    提取公共模块

---
+++ common/src/main/java/com/zzfn/Test.java
@@ -0,0 +1,36 @@
+package com.zzfn;
+
+public class Test {
+    private String id;
+
+    private String name;
+
+    private String age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+
+
+}
--- user/src/main/java/com/zzfn/TeatController.java
+++ user/src/main/java/com/zzfn/TeatController.java
@@ -1,10 +1,7 @@
 package com.zzfn;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author zzfn
