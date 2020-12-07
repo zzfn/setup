d2c54eeef7d971e052b45805c087b7293616c8fb
chenc <chenc@fengyuntec.com>
2020-12-07 09:24:56 +0800

    feat

--- user/src/main/java/com/zzfn/UserApplication.java
+++ user/src/main/java/com/zzfn/UserApplication.java
@@ -2,10 +2,7 @@ package com.zzfn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author zzfn
@@ -14,11 +11,6 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 public class UserApplication {
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
